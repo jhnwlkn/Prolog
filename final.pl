d9f18d5c9e64f260071341be15d78564bad78a37
John Wilken Christopher <92281847+jhnwlkn@users.noreply.github.com>
2023-06-19 19:11:39 +0700

    Add files via upload

---
+++ final.pl
@@ -0,0 +1,116 @@
+% Knowledge base
+film(joker, [crime, drama], [joaquin_phoenix], [twisted_plot], [unique_editing]).
+film(inception, [action, mystery, sci-fi], [leonardo_dicaprio], [complex_plot], [creative_editing]).
+film(the_shawshank_redemption, [drama], [tim_robbins, morgan_freeman], [inspiring_plot], [seamless_editing]).
+film(pulp_fiction, [crime, drama], [john_travolta, samuel_l_jackson], [non-linear_plot], [stylish_editing]).
+film(the_dark_knight, [action, crime, drama], [christian_bale, heath_ledger], [intense_plot], [dynamic_editing]).
+film(fight_club, [drama, thriller], [brad_pitt, edward_norton], [mind_bending_plot], [gritty_editing]).
+film(interstellar, [adventure, drama, sci-fi], [matthew_mcconaughey], [epic_plot], [visual_editing]).
+film(the_matrix, [action, sci-fi], [keanu_reeves, laurence_fishburne], [virtual_reality_plot], [slick_editing]).
+film(whiplash, [drama, music], [miles_teller, j_k_simmons], [intense_plot], [rhythmic_editing]).
+film(gone_girl, [drama, mystery, thriller], [ben_affleck, rosamund_pike], [twisted_plot], [suspenseful_editing]).
+film(the_social_network, [biography, drama], [jesse_eisenberg, andrew_garfield], [real_life_plot], [fast_paced_editing]).
+film(black_swam, [drama, thriller], [natalie_portman], [psychological_plot], [intricate_editing]).
+film(avatar, [action, adventure, fantasy, sci-fi], [sam_worthington, zoe_saldana], [epic_plot], [stunning_editing]).
+film(eternal_sunshine_of_the_spotless_mind, [drama, romance, sci-fi], [jim_carrey, kate_winslet], [mind_bending_plot], [imaginative_editing]).
+film(gladiator, [action, drama], [russell_crowe], [epic_plot], [seamless_editing]).
+film(goodfellas, [biography, crime, drama], [robert_de_niro, joe_pesci], [gangster_plot], [dynamic_editing]).
+film(frozen, [animation, adventure, comedy, family, fantasy, musical], [kristen_bell, idina_menzel], [heartwarming_plot], [captivating_editing]).
+film(iron_man, [action, adventure, sci-fi], [robert_downey_jr], [heroic_plot], [energetic_editing]).
+film(the_lion_king, [animation, adventure, drama, family, musical], [matthew_broderick, jeremy_irons], [epic_plot], [visually_stunning_editing]).
+film(the_hateful_eight, [crime, drama, mystery, thriller, western], [samuel_l_jackson, kurt_russell], [tense_plot], [masterful_editing]).
+film(black_panther, [action, adventure, sci-fi], [chadwick_boseman], [superhero_plot], [visually_stunning_editing]).
+film(parasite, [comedy, drama, thriller], [song_kang_ho], [social_satire_plot], [clever_editing]).
+film(tenet, [action, sci-fi, thriller], [john_david_washington], [time_bending_plot], [mind-bending_editing]).
+film(the_irishman, [biography, crime, drama], [robert_de_niro, al_pacino], [gangster_plot], [seamless_editing]).
+film(roma, [drama], [yalitza_aparicio], [personal_story_plot], [artistic_editing]).
+film(birdman, [comedy, drama], [michael_keaton], [meta_fiction_plot], [continuous_editing]).
+film(la_la_land, [comedy, drama, music, romance], [ryan_gosling, emma_stone], [musical_plot], [visually_stunning_editing]).
+film(moonlight, [drama], [trevante_rhodes, mahershala_ali], [coming_of_age_plot], [subtle_editing]).
+film(blade_runner_2049, [action, drama, sci-fi, thriller], [ryan_gosling, harrison_ford], [neo-noir_plot], [visually_stunning_editing]).
+film(get_out, [horror, mystery, thriller], [daniel_kaluuya], [social_thriller_plot], [tense_editing]).
+film(her, [drama, romance, sci-fi], [joaquin_phoenix, scarlett_johansson], [futuristic_love_story_plot], [subtle_editing]).
+film(call_me_by_your_name, [drama, romance], [timothee_chalamet, armie_hammer], [coming_of_age_plot], [captivating_editing]).
+film(black_mirror_bandersnatch, [drama, mystery, sci-fi, thriller], [fionn_whitehead], [interactive_plot], [unique_editing]).
+film(the_grand_budapest_hotel, [adventure, comedy, crime], [ralph_fiennes], [quirky_plot], [stylish_editing]).
+film(once_upon_a_time_in_hollywood, [comedy, drama], [leonardo_dicaprio, brad_pitt], [hollywood_story_plot], [nostalgic_editing]).
+film(palm_springs, [comedy, mystery, romance, sci-fi], [andy_samberg, cristin_milioti], [time_loop_plot], [clever_editing]).
+film(two_popes, [biography, comedy, drama], [anthony_hopkins, jonathan_pryce], [historical_plot], [thought-provoking_editing]).
+film(promising_young_woman, [crime, drama, thriller], [carey_mulligan], [revenge_plot], [bold_editing]).
+film(sound_of_metal, [drama, music], [riz_ahmed], [journey_of_self_discovery_plot], [immersive_editing]).
+film(minari, [drama], [steven_yeun], [family_drama_plot], [captivating_editing]).
+film(soul, [animation, adventure, comedy, family, fantasy, music], [jamie_foxx], [philosophical_plot], [visually_stunning_editing]).
+film(nomadland, [drama], [frances_mcdormand], [nomadic_life_plot], [minimalistic_editing]).
+film(the_father, [drama], [anthony_hopkins], [family_drama_plot], [disorienting_editing]).
+film(the_trial_of_the_chicago_7, [drama, history, thriller], [eddie_redmayne, sacha_baron_cohen], [legal_drama_plot], [fast-paced_editing]).
+film(mank, [biography, drama], [gary_oldman], [behind_the_scenes_plot], [nostalgic_editing]).
+film(pieces_of_a_woman, [drama], [vanessa_kirby], [emotional_journey_plot], [raw_editing]).
+film(judas_and_the_black_messiah, [biography, drama, history], [daniel_kaluuya, lakeith_stanfield], [political_plot], [powerful_editing]).
+film(druk_another_round, [comedy, drama], [mads_mikkelsen], [midlife_crisis_plot], [dynamic_editing]).
+film(one_night_in_miami, [drama], [kingsley_ben_adir, eli_goree], [historical_plot], [intimate_editing]).
+film(the_white_tiger, [crime, drama], [adarsh_gourav], [social_commentary_plot], [visually_arresting_editing]).
+film(soul, [animation, adventure, comedy, drama, family, fantasy, music], [jamie_foxx, tina_fey], [existential_plot], [visually_stunning_editing]).
+film(tenet, [action, sci-fi, thriller], [john_david_washington, robert_pattinson], [time_inversion_plot], [mind-bending_editing]).
+film(the_queens_gambit, [drama], [anya_taylor-joy], [chess_plot], [captivating_editing]).
+film(the_mauritanian, [drama, thriller], [jodie_foster, tahar_rahim], [legal_drama_plot], [tense_editing]).
+film(nomadland, [drama], [frances_mcdormand], [nomadic_life_plot], [minimalistic_editing]).
+film(the_father, [drama], [anthony_hopkins], [family_drama_plot], [disorienting_editing]).
+film(the_trial_of_the_chicago_7, [drama, history, thriller], [eddie_redmayne, sacha_baron_cohen], [legal_drama_plot], [fast-paced_editing]).
+film(mank, [biography, drama], [gary_oldman], [behind_the_scenes_plot], [nostalgic_editing]).
+film(pieces_of_a_woman, [drama], [vanessa_kirby], [emotional_journey_plot], [raw_editing]).
+film(judas_and_the_black_messiah, [biography, drama, history], [daniel_kaluuya, lakeith_stanfield], [political_plot], [powerful_editing]).
+film(druk_another_round, [comedy, drama], [mads_mikkelsen], [midlife_crisis_plot], [dynamic_editing]).
+film(one_night_in_miami, [drama], [kingsley_ben_adir, eli_goree], [historical_plot], [intimate_editing]).
+film(the_white_tiger, [crime, drama], [adarsh_gourav], [social_commentary_plot], [visually_arresting_editing]).
+film(soul, [animation, adventure, comedy, drama, family, fantasy, music], [jamie_foxx, tina_fey], [existential_plot], [visually_stunning_editing]).
+film(tenet, [action, sci-fi, thriller], [john_david_washington, robert_pattinson], [time_inversion_plot], [mind-bending_editing]).
+film(promising_young_woman, [crime, drama, thriller], [carey_mulligan], [revenge_plot], [bold_editing]).
+film(sound_of_metal, [drama, music], [riz_ahmed], [journey_of_self_discovery_plot], [immersive_editing]).
+film(minari, [drama], [steven_yeun], [family_drama_plot], [captivating_editing]).
+film(ma_raineys_black_bottom, [drama, music], [chadwick_boseman, viola_davis], [music_industry_plot], [energetic_editing]).
+film(palm_springs, [comedy, mystery, romance, sci-fi], [andy_samberg, cristin_milioti], [time_loop_plot], [clever_editing]).
+film(two_popes, [biography, comedy, drama], [anthony_hopkins, jonathan_pryce], [historical_plot], [thought-provoking_editing]).
+film(the1917, [drama, war], [george_mackay, dean-charles_chapman], [world_war_i_plot], [immersive_editing]).
+film(joker, [crime, drama, thriller], [joaquin_phoenix], [character_study_plot], [gritty_editing]).
+film(parasite, [comedy, drama, thriller], [song_kang_ho], [social_satire_plot], [clever_editing]).
+film(marriage_story, [comedy, drama, romance], [scarlett_johansson, adam_driver], [relationship_drama_plot], [emotional_editing]).
+film(the_irishman, [biography, crime, drama], [robert_de_niro, al_pacino, joe_pesci], [gangster_plot], [seamless_editing]).
+film(little_women, [drama, romance], [saoirse_ronan, emma_watson, florence_pugh], [coming_of_age_plot], [nostalgic_editing]).
+film(jojo_rabbit, [comedy, drama, war], [roman_griffin_davis, thomasin_mckenzie], [world_war_ii_plot], [satirical_editing]).
+film(once_upon_a_time_in_hollywood, [comedy, drama], [leonardo_dicaprio, brad_pitt], [hollywood_story_plot], [nostalgic_editing]).
+
+
+% Question
+recommend_movie :-
+    write('Apa genre film yang Anda inginkan? '), nl,
+    read(Genre),
+    write('Siapa aktor yang Anda sukai? '), nl,
+    read(Actor),
+    write('Apakah Anda ingin alur cerita yang rumit? (yes/no) '), nl,
+    read(Plot),
+    write('Apakah Anda menginginkan editing yang unik? (yes/no) '), nl,
+    read(Editing),
+    find_movie(Genre, Actor, Plot, Editing, Movie),
+    write('Berdasarkan preferensi Anda, saya merekomendasikan film: '), write(Movie), nl.
+
+% Search
+find_movie(Genre, Actor, Plot, Editing, Movie) :-
+    film(Movie, Genres, Actors, Plots, Editings),
+    match_genre(Genre, Genres),
+    match_actor(Actor, Actors),
+    match_plot(Plot, Plots),
+    match_editing(Editing, Editings).
+
+% Matching genre
+match_genre(Genre, Genres) :-
+    member(Genre, Genres).
+
+% Matching actor
+match_actor(Actor, Actors) :-
+    member(Actor, Actors).
+
+% Matching plot
+match_plot(yes, _).
+
+% Matching edit
+match_editing(yes, _).
